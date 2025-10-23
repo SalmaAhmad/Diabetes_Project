a87b18de32732bc63e0aaa6c4b1beb7945f6d604
SalmaAhmad <salmasameer1000@gmail.com>
2025-10-23 21:19:05 +0300

    my initial code

---
+++ Project.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, minmax_scale
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv("Dataset_Diabetes.csv")
+
+#Step 1: Understanding the dataset
+#print(df.head())
+print(df.info())
+#print(df.describe())
+
+#Step2: Null
+#check for null values even though df.info confirmed there arent any
+print(df.isnull().sum())
+#Step3: Duplicates
+#check for duplicated rows
+print(df.duplicated().sum())
+
+#Step4: Categorical Columns (Class & Gender)
+cat_col=df.select_dtypes(include = 'object').columns
+print (cat_col)
+
+for col in cat_col:
+    print(df[col].unique())
+
+df['Gender'] = df['Gender'].replace({'f':'F'})
+df['CLASS'] = df['CLASS'].replace({'N ':'N','Y ':'Y'})
+
+for col in cat_col:
+    print(df[col].unique())
+
+#now we have consistent values
+#label encode Gender
+gender_encoder= LabelEncoder()
+df['Gender_encoded']=gender_encoder.fit_transform(df['Gender'])
+#One hot encode Class
+df=pd.get_dummies(df,columns=['CLASS'],prefix='Class',dtype=int)
+
+
+#print(df)
+
+#drop id and number of patients because they will distort the results
+num_col=df.select_dtypes(exclude='object').columns.drop(['ID', 'No_Pation','Gender_encoded','Class_N','Class_P','Class_Y'])
+print (num_col)
+
+#now we check for outliers
+q1= df[num_col].quantile(0.25)
+q3 = df[num_col].quantile(0.75)
+iqr= q3-q1
+
+outlier_mask = (df[num_col] < (q1 - 1.5 * iqr)) | (df[num_col] > (q3 + 1.5 * iqr))
+
+outlier_counts = outlier_mask.sum()
+print(outlier_counts)
+
+
+#visualizing the outliers
+df[num_col].boxplot(figsize=(12,6))
+plt.xticks(rotation=45)
+plt.show()
+
+df_cleaned = df[~outlier_mask.any(axis=1)]
+
+#visualizing after removing outliers
+df_cleaned.boxplot(figsize=(12,6))
+plt.xticks(rotation=45)
+plt.show()
+
+#now scaling
+scaler=MinMaxScaler()
+df_cleaned[num_col] = scaler.fit_transform(df_cleaned[num_col])
+df_cleaned[num_col].describe()
+
+df_cleaned[num_col].hist(bins=20, figsize=(15,10))
+plt.suptitle("Histogram of Scaled Features")
+plt.show()
+
+plt.figure(figsize=(12,8))
+sns.heatmap(df_cleaned[num_col].corr(), annot=True, cmap='coolwarm')
+plt.title("Correlation Heatmap of Scaled Features")
+plt.show()
